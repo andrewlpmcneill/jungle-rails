e74a8a215f41b6eb6cc65e78e2965439739d3366
Andrew McNeill <andrew@andrewmc.ca>
2022-03-14 23:27:38 -0300

    adds validations, adds authenticate_with_credentials class method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,21 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  
+  def self.authenticate_with_credentials(email, password)
+    email = email.split(' ')[0].downcase
+    user = User.find_by_email(email)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
+  
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 8 }
+  validates :password_confirmation, presence: true
+  before_save { self.email = email.downcase }
+  
 end
